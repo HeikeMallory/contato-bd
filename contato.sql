cdbaba02bc7cea1e0abf739712de00121c51ced0
Bruna Cabezas <brunacabezas94@gmail.com>
2023-10-26 22:03:40 -0300

    criação tabela 'lista'

--- contato.sql
+++ contato.sql
@@ -1,3 +1,12 @@
 -- criar banco de dados contato
 CREATE DATABASE contato;
+
+-- Comando de seleção de banco
 USE contato;
+
+-- Criar uma tabela chamada 'lista' com 3 campos: ID auto incremental, nome da pessoa e o telefone dela. 
+CREATE TABLE lista (
+id INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(50) NOT NULL,
+telefone VARCHAR(50)
+);
